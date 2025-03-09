8cb36ddadb6fcd6acc0d8abcc440e50a6916d503
y423610m <y423610m@gmail.com>
2025-03-09 21:16:08 +0900

    remove unused buttons

--- src/app/TopBar.tsx
+++ src/app/TopBar.tsx
@@ -8,8 +8,6 @@ const TopBar: React.FC = () => {
         <div className="logo">MyApp</div>
         <div className="navLinks">
           <a href="/" className="navButton">Home</a>
-          <a href="/teaching-control" className="navButton">TeachingControl</a>
-          <a href="/" className="navButton">TODO</a>
         </div>
       </div>
     </div>
